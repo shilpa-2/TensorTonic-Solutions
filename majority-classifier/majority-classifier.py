8cb2ae728ac62d47841320b77f9dd90c041af4ce
SHILPA KUMARI <shilpa4601@gmail.com>
2026-04-21 14:35:42 +0530

    Update solution for majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -5,7 +5,6 @@ def majority_classifier(y_train, X_test):
     Predict the most frequent label in training data for all test samples.
     """
     # Write code here
-
     values, counts = np.unique(y_train, return_counts=True)
     majority_class = values[np.argmax(counts)]
     return np.full(len(X_test), majority_class)
